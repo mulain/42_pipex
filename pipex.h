f7dd4999c6ae9661d8d223fcb28e4e5da1e172b5
wolf <wolf.mardin@gmail.com>
2022-09-16 12:23:01 +0200

    auto add & push

---
+++ pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 10:20:42 by wmardin           #+#    #+#             */
+/*   Updated: 2022/09/16 10:23:03 by wmardin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+
+# define PIPEX_H
+# include "libft/include/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+
+# include <unistd.h>
+# include <sys/errno.h>
+# include <string.h>
+
+#endif
