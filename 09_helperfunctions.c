d72bc8fe461db4a780e91a33cb46fc5d9d2a8e19
wolf <wolf.mardin@gmail.com>
2022-09-18 23:29:28 +0200

    auto add & push

--- 09_helperfunctions.c
+++ 09_helperfunctions.c
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 19:50:17 by wmardin           #+#    #+#             */
-/*   Updated: 2022/09/18 23:12:56 by wmardin          ###   ########.fr       */
+/*   Updated: 2022/09/18 23:20:21 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ void	print2d(char **array)
 	}
 }
 
-void	free3darray(char ***array)
+void	free3d(char ***array)
 {
 	int		i;
 	int		j;
@@ -68,6 +68,19 @@ void	free3darray(char ***array)
 		free(array[j]);
 		i = 0;
 		j++;
+	}
+	free(array);
+}
+
+void	free2d(char **array)
+{
+	int		i;
+
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
 	}
 	free(array);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 10:20:42 by wmardin           #+#    #+#             */
-/*   Updated: 2022/09/18 23:13:23 by wmardin          ###   ########.fr       */
+/*   Updated: 2022/09/18 23:18:39 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,6 @@ void	error_path(void);
 //09_helperfunctions.c
 void	print3d(char ***array);
 void	print2d(char **array);
-void	free3darray(char ***array);
-
+void	free3d(char ***array);
+void	free2d(char **array);
 #endif
